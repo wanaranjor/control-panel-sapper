a35c144813c044b4aa71c800179e62310301bb3b
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-02-14 19:10:57 -0500

    add firebase.js to .gitignore

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,7 +1,3 @@
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
-// import 'firebase/firestore'
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDjiZWG68DCvmfTpTmSmVYu4vq2UPWczjE",
@@ -13,6 +9,3 @@ const firebaseConfig = {
   appId: "1:921582866810:web:4bff0004ab5bff239b46c3"
 };
 firebase.initializeApp(firebaseConfig);
-
-// export const db = firebase.firestore();
-// export const auth = firebase.auth();

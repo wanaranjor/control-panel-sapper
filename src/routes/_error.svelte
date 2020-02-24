c095ab9f6df4fcadd53a8092ec2c903d089d13a4
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-02-24 18:35:23 -0500

    update error page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,6 +1,7 @@
 <script>
+	import Logo404 from '../components/UI/404.svelte';
 	export let status;
-	export let error;
+	// export let error;
 
 	const dev = process.env.NODE_ENV === 'development';
 </script>
@@ -9,10 +10,18 @@
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<!-- <h1>{status}</h1>
 
 <p>{error.message}</p>
 
 {#if dev && error.stack}
 	<pre>{error.stack}</pre>
-{/if}
+{/if} -->
+
+<div class="container mx-auto">
+	<div class="flex flex-col text-5xl text-center font-semibold items-center">
+		<h1>404</h1>
+		<Logo404 />
+		<p>Algo anda mal</p>
+	</div>
+</div>

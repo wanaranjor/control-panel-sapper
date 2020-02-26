b63f7a0b848298cb76af32f58c3e530e67b83191
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2020-02-25 22:52:57 -0500

    update index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,15 +13,17 @@
 </svelte:head>
 
 {#if !$currentUser}
-
-<div class="grid grid-cols-3 gap-4">
-  <div>
-    <div class="hidden md:block">
-        <a href="/login" class="inline-block py-1 md:py-4 text-gray-500 hover:text-gray-100 mr-6">Login</a>
-        <!-- <a href="/login" class="inline-block py-2 px-4 text-gray-700 bg-white hover:bg-gray-100 rounded-lg">Sign Up</a > -->
+  <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl">
+    <img class="h-32" src="https://picsum.photos/400/100" alt="2020">
+    <img class="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center" src="https://picsum.photos/1600/300" alt="tools">
+    <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight sm:mt-8 sm:text-4xl ">Quiero ir a la playa a 
+      <span class="text-orange-700">el mar azul.</span>
+    </h1>
+    <p class="mt-2 text-gray-700 sm:mt-4 sm:text-xl">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae velit nostrum totam aperiam dicta, adipisci, obcaecati provident consequatur nesciunt harum ipsum assumenda, pariatur quidem vitae libero modi tempora. At, inventore.
+    </p>
+    <div class="mt-4 sm:mt-6">
+      <a href="/login" class="inline-block px-5 py-3 rounded-lg shadow-lg bg-orange-600 text-sm text-white uppercase tracking-wider font-semibold sm:text-base"> Contacteme</a>
     </div>
   </div>
-  <!-- ... -->
-  <div>9</div>
-</div>
 {/if}
